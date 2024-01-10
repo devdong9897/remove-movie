890d880af8de7675d38368d2b0590cd4f1a9b8b9
devdong9897 <dev.dong9897@gmail.com>
2024-01-10 15:58:04 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from '../core/heropy';
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'DongWook',
+  email: 'dev.dong9897@gmail.com',
+  github: 'https://github.com/devdong9897?tab=repositories',
+  repository: 'https://github.com/devdong9897?tab=repositories/vanilajs-movie-app',
+});
